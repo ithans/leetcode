9ddf34e1f6535f1e08442c5cf3789664916a5979
handong <18092230501@163.com>
2020-04-20 13:47:08 +0800

    finish leetcode21 test

---
+++ src/easy/Leetcode21.java
@@ -0,0 +1,28 @@
+package easy;
+
+//将两个升序链表合并为一个新的升序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+public class Leetcode21 {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        }
+        if (l1.val <= l2.val) {
+            l1.next = mergeTwoLists(l1.next,l2);
+            return l1;
+        }else{
+            l2.next = mergeTwoLists(l1,l2.next);
+            return l2;
+        }
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
