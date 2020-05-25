b68a4e7232aa03c3099cde87321c9dd14ca20cef
handong <18092230501@163.com>
2020-05-25 23:56:34 +0800

    finish leetcode70 test

---
+++ src/easy/Test70.java
@@ -0,0 +1,21 @@
+package easy;
+
+//假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+//每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+public class Test70 {
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        } else if (n == 2) {
+            return 2;
+        } else {
+            int[] arr = new int[n];
+            arr[0] = 1;
+            arr[1] = 2;
+            for (int i = 2; i < n; i++) {
+                arr[i] = arr[i - 1] + arr[i - 2];
+            }
+            return arr[n - 1];
+        }
+    }
+}
